f1aeb4c8f006df5e8fed824f773952ab4d9b4e1c
Yibao Liu <734988964@qq.com>
2016-12-29 13:57:07 +0800

    update

---
+++ struct/02.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+type Circle struct {
+	X, Y, Radius int
+}
+
+type Wheel struct {
+	X, Y, Radius, Spokes int
+}
+
+func main() {
+	var w Wheel
+	w.X = 8
+	w.Y = 8
+	w.Radius = 5
+	w.Spokes = 20
+	fmt.Println(w)
+}
