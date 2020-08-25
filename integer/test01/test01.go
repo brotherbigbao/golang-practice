514167bd93dec7dd8544e2de93947fff5a2f0331
liuyibao <734988964@qq.com>
2020-08-25 14:21:31 +0800

    update

---
+++ integer/test01/test01.go
@@ -0,0 +1,16 @@
+package main
+
+const name = 1
+
+func main() {
+	var myName uint32 = 1
+	if myName == name {
+		print("yes")
+	} else {
+		print("no")
+	}
+
+	if bool(myName) {
+
+	}
+}
---
+++ integer/test02/test02.go
@@ -0,0 +1,6 @@
+package main
+
+func main() {
+	var a uint32
+	print(a)
+}
