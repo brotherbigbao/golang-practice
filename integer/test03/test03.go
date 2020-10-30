36a678363807067f318483f87296cdf913afbecc
liuyibao <734988964@qq.com>
2020-10-30 10:50:06 +0800

    struct

---
+++ integer/test03/test03.go
@@ -0,0 +1,8 @@
+package main
+
+func main() {
+	a := 3
+    b := a & 2
+    print(b)
+
+}
---
+++ struct/struct01/01.go
@@ -0,0 +1,11 @@
+package main
+
+type Point struct {
+	X, Y int
+}
+
+func main() {
+	var a Point
+	print(a.X)
+	print(a.Y)
+}
