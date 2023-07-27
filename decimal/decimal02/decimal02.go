dd1560d9a28f15ce8ad17355561a2af260d9dcfe
liuyibao <734988964@qq.com>
2023-07-27 17:53:27 +0800

    update

--- decimal/decimal02/decimal02.go
+++ decimal/decimal02/decimal02.go
@@ -3,11 +3,16 @@ package main
 import "github.com/shopspring/decimal"
 
 func main() {
-	price, err := decimal.NewFromString("3.1015926");
+	price, err := decimal.NewFromString("0.0");
 	if err != nil {
 		println(err)
 	}
 	println(price.Float64())
 
 	println(price.RoundDown(1).Float64())
+
+	floatNum, _ := price.RoundDown(1).Float64()
+	if floatNum == 0 {
+		println("=0")
+	}
 }
