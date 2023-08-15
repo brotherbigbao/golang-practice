41374edcbbb8a0e36077a4f9943a01286e34a49e
liuyibao <734988964@qq.com>
2023-08-15 17:38:46 +0800

    测试 decimal round

---
+++ decimal/decimal05/decimal05.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"github.com/shopspring/decimal"
+)
+
+func main() {
+	decimalRoundDebug("3.01")
+	fmt.Println("==========")
+	decimalRoundDebug("3.05")
+	fmt.Println("==========")
+	decimalRoundDebug("3.09")
+	fmt.Println("==========")
+}
+
+func decimalRoundDebug(number string)  {
+	d, err := decimal.NewFromString(number)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println(d.Round(1).String())
+	fmt.Println(d.RoundUp(1).String())
+	fmt.Println(d.RoundDown(1).String())
+	return
+}
